ef2f66bb23df1ee770546fc1d13137f590610320
Albertka <108755008+AalbertkaA@users.noreply.github.com>
2023-09-12 15:13:12 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#BOT_TOKEN = str(os.getenv(("BOT_TOKEN")))
+BOT_TOKEN = "6253941211:AAGmn8ndJnZVzWewsKsA8B-Sr73D4d--zQ0"
+
+admins_id = [
+    5844616297
+]
+
+chatAdminTours = '-957615428'
+
+activeStatus = ["user", "super"]
